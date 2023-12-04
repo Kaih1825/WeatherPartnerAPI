580ad86f2cf6b759587b7f8bd9d1cb9f2eb52739
Kai <imatch181@gmail.com>
2023-12-04 10:37:40 +0800

    Remove Weather Partner website

--- main.py
+++ main.py
@@ -25,26 +25,9 @@ cursor.execute(create_table_sql)
 @app.route('/')
 def info():
     # return "Weather Partner API Server"
-    return render_template("index_api.html")
+    return render_template("index.html")
 
 FLUTTER_WEB_APP = 'templates'
-
-@app.route('/web/')
-def render_page_web():
-    return render_template('index.html')
-
-
-@app.route('/web/<path:name>')
-def return_flutter_doc(name):
-
-    datalist = str(name).split('/')
-    DIR_NAME = FLUTTER_WEB_APP
-
-    if len(datalist) > 1:
-        for i in range(0, len(datalist) - 1):
-            DIR_NAME += '/' + datalist[i]
-
-    return send_from_directory(DIR_NAME, datalist[-1])
 
 #Post data
 @app.post("/data")
